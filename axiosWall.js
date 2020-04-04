a602bcc971edd9ef30bbb89b884ef262c8e7cdbf
seid zerihun <seidoo99@gmial.com>
2020-04-04 11:16:44 -0400

    second change

--- axiosWall.js
+++ axiosWall.js
@@ -1,33 +1,44 @@
-var download = require('download-file')
+const axios = require('axios');
+const download = require('download-file');
 const wallpaper = require('wallpaper');
-const axios = require('axios')
-var url = 'https://dog.ceo/api/breeds/image/random';
-axios.get(url).then((response) => {
-    var data = response.data
-    return data;
-}).catch(error => {
-    return error
-})
+const urlMain = 'https://dog.ceo/api/breeds/image/random';
 
+axios.get(urlMain)
+    .then(function(response) {
+        var url = response.data.message
+        var options = {
+                directory: "./images/dogs/",
+                filename: "dog.gif"
+            }
+            // download(url, options, async function(err) {
+            //     if (err) throw err
+            //     console.log("meow");
+            //     return await wallpaper.set('./images/dogs/dog.gif')
+            // })
+        download(url, options, function(err) {
+            if (err) {
+                throw err
+            } else
+                console.log("meow");
+            return wallpaper.set('./images/dogs/dog.gif')
+        })
+
+    })
+    .catch(function(error) {
+        console.log(error);
+    });
+/*
 var options = {
-    directory: "./images/dogs/",
-    filename: "dog.gif"
-}
-
-download(url, options, function(err, data) {
-    if (err) {
-        throw err;
-    } else {
-        return data
-    }
-
-})
-
-
-
-// (async() => {
-//     await wallpaper.set('dog.gif);
-
-//     await wallpaper.get();
-//     //=> '/Users/sindresorhus/unicorn.jpg'
-// })();
+                filename: "dog.gif"
+            }
+    axios
+    .get("https://dog.ceo/api/breeds/image/random")
+    .then(function (response) {
+      url = response.data.message;
+      download(url, options);
+      wallpaper.set("./dog.jpg");
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
+    */
